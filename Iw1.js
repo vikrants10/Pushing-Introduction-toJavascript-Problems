9a1b5d6d4bc255af8b115cd432acd2ae47066a5f
vikrant10 <vikrantkumarsingh333@gmail.com>
2022-11-01 19:16:46 +0530

    done

---
+++ Iw1.js
@@ -0,0 +1,12 @@
+//rahul purchase 4000
+//discount at 3999 of 10%
+let bill = 4000;
+
+let min_pur = 3999;
+
+
+if (bill >= min_pur) {
+  console.log("the new bill is", bill -( bill * 0.1));
+} else {
+  console.log("not eligible");
+} 
